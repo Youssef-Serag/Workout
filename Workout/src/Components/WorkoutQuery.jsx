026c8befd157cc284c797abe11f6b5117d4718e2
Youssef Serag <yserag@asu.edu>
2023-12-31 00:24:22 -0500

    Completed the form to get data.

---
+++ Workout/src/Components/WorkoutQuery.jsx
@@ -0,0 +1,131 @@
+import React, { useState } from "react";
+import {
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Box,
+  Button,
+  Grid,
+} from "@mui/material";
+
+const WorkoutQuery = () => {
+  const [dropdownValue2, setDropdownValue2] = useState("");
+  const [dropdownValue3, setDropdownValue3] = useState("");
+  const [dropdownValue4, setDropdownValue4] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(dropdownValue2, dropdownValue3, dropdownValue4);
+
+    fetch(
+      `https://api.api-ninjas.com/v1/exercises?type=${dropdownValue2}&muscle=${dropdownValue3}&difficulty=${dropdownValue4}`,
+      {
+        headers: {
+          "X-Api-Key": "token",
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error("Request failed:", error));
+  };
+
+  return (
+    <Box
+      sx={{
+        backgroundColor: "#f5f5f5",
+        padding: 3,
+        borderRadius: 2,
+        display: "flex",
+      }}
+    >
+      <form>
+        <Grid container direction="row" alignItems="center">
+          <Grid item>
+            <FormControl sx={{ m: 1, minWidth: 100, flexGrow: 1 }}>
+              <InputLabel id="dropdown2-label">Type</InputLabel>
+              <Select
+                labelId="Type-label"
+                value={dropdownValue2}
+                label="Type"
+                onChange={(e) => setDropdownValue2(e.target.value)}
+              >
+                <MenuItem disabled value="">
+                  <em>Type</em>
+                </MenuItem>
+                <MenuItem value={"cardio"}>Cardio</MenuItem>
+                <MenuItem value={"olympic_weightlifting"}>
+                  Olmpic Weightlifting
+                </MenuItem>
+                <MenuItem value={"plyometrics"}>Ploymetrics</MenuItem>
+                <MenuItem value={"powerlifting"}>Powerlifting</MenuItem>
+                <MenuItem value={"strength"}>Strength</MenuItem>
+                <MenuItem value={"stretching"}>Stretching</MenuItem>
+                <MenuItem value={"strongman"}>Strongman</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+
+          <Grid item>
+            <FormControl sx={{ m: 1, minWidth: 100, flexGrow: 1 }}>
+              <InputLabel id="muscle-label">Muscle</InputLabel>
+              <Select
+                labelId="muscle-label"
+                value={dropdownValue3}
+                label="Muscle"
+                onChange={(e) => setDropdownValue3(e.target.value)}
+              >
+                <MenuItem disabled value="">
+                  <em>Muscle</em>
+                </MenuItem>
+                <MenuItem value={"abdominals"}>Abdominals</MenuItem>
+                <MenuItem value={"abductors"}>Abductors</MenuItem>
+                <MenuItem value={"adductors"}>Adductors</MenuItem>
+                <MenuItem value={"biceps"}>Biceps</MenuItem>
+                <MenuItem value={"calves"}>Calves</MenuItem>
+                <MenuItem value={"chest"}>Chest</MenuItem>
+                <MenuItem value={"forearms"}>Forearms</MenuItem>
+                <MenuItem value={"glutes"}>Glutes</MenuItem>
+                <MenuItem value={"hamstrings"}>Hamstrings</MenuItem>
+                <MenuItem value={"lats"}>Lats</MenuItem>
+                <MenuItem value={"lower_back"}>Lower Back</MenuItem>
+                <MenuItem value={"middle_back"}>Middle Back</MenuItem>
+                <MenuItem value={"neck"}>Neck</MenuItem>
+                <MenuItem value={"quadriceps"}>Quadriceps</MenuItem>
+                <MenuItem value={"traps"}>Traps</MenuItem>
+                <MenuItem value={"triceps"}>Triceps</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+
+          <Grid item>
+            <FormControl sx={{ m: 1, minWidth: 100, flexGrow: 1 }}>
+              <InputLabel id="difficulty-label">Difficulty</InputLabel>
+              <Select
+                labelId="difficulty-label"
+                value={dropdownValue4}
+                label="Difficulty"
+                onChange={(e) => setDropdownValue4(e.target.value)}
+              >
+                <MenuItem disabled value="">
+                  <em>Difficulty</em>
+                </MenuItem>
+                <MenuItem value={"beginner"}>Beginner</MenuItem>
+                <MenuItem value={"intermediate"}>Intermediate</MenuItem>
+                <MenuItem value={"expert"}>Expert</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item>
+            <Button variant="contained" color="success" onClick={handleSubmit}>
+              Submit
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </Box>
+  );
+};
+
+export default WorkoutQuery;
--- Workout/src/Pages/WorkoutPage.jsx
+++ Workout/src/Pages/WorkoutPage.jsx
@@ -1,100 +1,14 @@
-import React, { useState } from 'react';
 import { Container, Typography, Box, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
+import WorkoutQuery from '../Components/WorkoutQuery';
 
 const MainPage = () => {
-  const [dropdownValue1, setDropdownValue1] = useState('');
-  const [dropdownValue2, setDropdownValue2] = useState('');
-  const [dropdownValue3, setDropdownValue3] = useState('');
-  const [dropdownValue4, setDropdownValue4] = useState('');
-  const [dropdownValue5, setDropdownValue5] = useState('');
 
   return (
-    <Container>
+    <Container container >
       <Typography variant="h2" align="center" gutterBottom>
         Page Title
       </Typography>
-
-      <Box sx={{ backgroundColor: '#f5f5f5', padding: 3, borderRadius: 2, display: 'flex'}} autoWidth>
-        <FormControl  sx={{ m: 1, minWidth: 100, flexGrow: 1 }}>
-          <InputLabel id="dropdown1-label">Name</InputLabel>
-          <Select
-            labelId="dropdown1-label"
-            value={dropdownValue1}
-            label="Dropdown 1"
-            onChange={(e) => setDropdownValue1(e.target.value)}
-            autoWidth
-          >
-            {/* Populate with menu items */}
-            <MenuItem value={10}>Option 1</MenuItem>
-            <MenuItem value={20}>Option 2</MenuItem>
-          </Select>
-        </FormControl>
-
-        <FormControl  sx={{ m: 1, minWidth: 100, flexGrow: 1 }}>
-          <InputLabel id="dropdown2-label">Type</InputLabel>
-          <Select
-            labelId="dropdown2-label"
-            value={dropdownValue2}
-            label="Dropdown 2"
-            onChange={(e) => setDropdownValue2(e.target.value)}
-            autoWidth
-          >
-            {/* Populate with menu items */}
-            <MenuItem value={10}>Option 1</MenuItem>
-            <MenuItem value={20}>Option 2</MenuItem>
-          </Select>
-        </FormControl>
-
-        <FormControl  sx={{ m: 1, minWidth: 100, flexGrow: 1 }}>
-          <InputLabel id="dropdown3-label">Muscle</InputLabel>
-          <Select
-            labelId="dropdown3-label"
-            value={dropdownValue3}
-            label="Dropdown 3"
-            onChange={(e) => setDropdownValue3(e.target.value)}
-            autoWidth
-          >
-            {/* Populate with menu items */}
-            <MenuItem value={10}>Option 1</MenuItem>
-            <MenuItem value={20}>Option 2</MenuItem>
-          </Select>
-        </FormControl>
-
-        <FormControl  sx={{ m: 1, minWidth: 100, flexGrow: 1 }}>
-          <InputLabel id="dropdown4-label">Difficulty</InputLabel>
-          <Select
-            labelId="dropdown4-label"
-            value={dropdownValue4}
-            label="Dropdown 4"
-            onChange={(e) => setDropdownValue4(e.target.value)}
-            autoWidth
-          >
-            {/* Populate with menu items */}
-            <MenuItem value={10}>Option 1</MenuItem>
-            <MenuItem value={20}>Option 2</MenuItem>
-          </Select>
-        </FormControl>
-      </Box>
-      <div>
-      <FormControl sx={{ m: 1, minWidth: 80 }}>
-        <InputLabel id="demo-simple-select-autowidth-label">Age</InputLabel>
-        <Select
-          labelId="demo-simple-select-autowidth-label"
-          id="demo-simple-select-autowidth"
-          value={dropdownValue5}
-          onChange={(e) => setDropdownValue5(e.target.value)}
-          autoWidth
-          label="Age"
-        >
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
-          <MenuItem value={10}>Twenty</MenuItem>
-          <MenuItem value={21}>Twenty one</MenuItem>
-          <MenuItem value={22}>Twenty one and a half</MenuItem>
-        </Select>
-      </FormControl>
-    </div>
+      <WorkoutQuery/>
     </Container>
   );
 };
